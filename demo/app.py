966433d3e075cab30cdce882713e4ba0e59060c8
Mini-Pingu <xxxdavidaksnesxxx@gmail.com>
2021-03-14 16:49:45 +0800

    stop @ Executing Modules as Scripts

---
+++ demo/app.py
@@ -0,0 +1,12 @@
+# from sales import calc_shipping, calc_tax
+
+
+from ecommerce.sales import calc_tax, calc_shipping
+from ecommerce import sales
+import sys
+
+print(sys.path)
+
+
+calc_shipping()
+calc_tax()
